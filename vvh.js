7efdf5bc0616bea62d1045a42b7ec87dda36b1df
Skyler Rojas <skylrroj0714@aol.com>
2013-02-25 14:30:42 -0800

    files to test vvh

---
+++ vvh.js
@@ -0,0 +1,7 @@
+var remoteness = {"status":"ok","response":[{"board":"X     X O","remoteness":4,"value":"win","move":"A1"},{"board":"   X  X O","remoteness":4,"value":"win","move":"A2"},{"board":"  X   X O","remoteness":4,"value":"win","move":"C1"},{"board":" X    X O","remoteness":6,"value":"tie","move":"B1"},{"board":"    X X O","remoteness":6,"value":"tie","move":"B2"},{"board":"     XX O","remoteness":6,"value":"tie","move":"C2"},{"board":"      XXO","remoteness":5,"value":"lose","move":"B3"}]};	
+function drawVVH(var1) {
+	var canvas = document.getElementById(var1);
+	var ctx = canvas.getContext("2d");
+	ctx.fillStyle="grey";
+	ctx.fillRect(0,0,150,75);
+}
